75dbdbb7ec85c00e51016b1b856a5cf3283e1df7
Amir Kidwai <findamirkidwai@gmail.com>
2024-09-06 20:05:08 +0530

    Update rhu.py

--- rhu.py
+++ rhu.py
@@ -24,10 +24,10 @@ def generate_lip_sync_data(audio_file_name):
     print(f"Generating lip-sync data for audio file: {audio_file_name}")
     
     # Define the working directory and the command
-    working_directory = '/Users/amir/desktop/rhubarb/rhu/lib/python3.12/site-packages'
+    working_directory = '/home/amir/pod/rhu/lib/python3.11/site-packages'
     output_file = 'output.json'
     
-    command = f'/Users/amir/Desktop/rhubarb/rhu/lib/python3.12/site-packages/Rhubarb-Lip-Sync-1.13.0-macOS/rhubarb -f json "{audio_file_name}" -o "{output_file}"'
+    command = f'/home/amir/pod/rhu/lib/python3.11/site-packages/Rhubarb-Lip-Sync-1.13.0-Linux/rhubarb -f json "{audio_file_name}" -o "{output_file}"'
 
     print(f"Executing command: {command}")
 
